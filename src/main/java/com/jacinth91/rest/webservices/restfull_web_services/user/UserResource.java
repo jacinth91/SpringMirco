da5b8cc7a271d0b58818aacb9d2aa5cd8ec6edd3
JacinthV <joy237@gmail.com>
2024-08-14 03:22:59 +0530

    added delete mapping

--- src/main/java/com/jacinth91/rest/webservices/restfull_web_services/user/UserResource.java
+++ src/main/java/com/jacinth91/rest/webservices/restfull_web_services/user/UserResource.java
@@ -6,7 +6,7 @@ import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import java.net.URI;
 import java.util.List;
-
+//dsdsdk
 @RestController
 public class UserResource {
 
